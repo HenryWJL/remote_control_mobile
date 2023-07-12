35acb62fc19b2650f591e6f8287a4fd29b1afa28
JunlinWang <12112921@mail.sustech.edu.cn>
2023-07-12 20:38:03 +0800

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -1,28 +1,38 @@
 import rospy
+import cv2
+import numpy as np
 from geometry_msgs.msg import Twist
+from sensor_msgs.msg import Image
 from socket import *
+from cv_bridge import CvBridge
 
+
+def callback(data):
+    global image
+    image = bridge.imgmsg_to_cv2(data, "bgr8")
+    
 
 if __name__ == '__main__':
+    image = None
     try:
         rospy.init_node("server", anonymous=True)
+        image_topic = rospy.get_param("image_topic", default="/camera/color/image_raw")
+        rospy.Subscriber(image_topic, Image, callback, queue_size=10)
         publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         
         listenSocket = socket(AF_INET, SOCK_STREAM)
-        listenSocket.bind(('127.0.0.1', 8000))
+        listenSocket.bind(('10.27.250.165', 8000))
         listenSocket.listen(1)
         print('The server has already started')
         dataSocket, address = listenSocket.accept()
         print('Connected to ', address)
         
-        while True:
+        while not rospy.is_shutdown():
             data = dataSocket.recv(512)
             if not data:
                 break
             
             command = data.decode('utf-8')
-            print(command)
-            
             if command == 'stop':
                 message = Twist()
                 publisher.publish(message)
@@ -55,5 +65,5 @@ if __name__ == '__main__':
         dataSocket.close()
         listenSocket.close()
         
-    except ConnectionError:
+    except (ConnectionError, OSError):
         print('Connection failed')
