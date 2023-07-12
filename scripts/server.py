6a05b9d1bb13cd6e26fdd6366f87dc0a917978fe
JunlinWang <12112921@mail.sustech.edu.cn>
2023-07-12 23:19:36 +0800

    Update server.py

--- scripts/server.py
+++ scripts/server.py
@@ -23,13 +23,18 @@ if __name__ == '__main__':
         listenSocket = socket(AF_INET, SOCK_STREAM)
         listenSocket.bind(('10.27.250.165', 8000))
         listenSocket.listen(1)
-        print('The server has already started')
+        rospy.loginfo('The server has already started')
         dataSocket, address = listenSocket.accept()
-        print('Connected to ', address)
+        rospy.loginfo('Connected to ', address)
         
         while not rospy.is_shutdown():
+            if not image:
+                rospy.logwarn('No image data available!')
+                continue
+                
             data = dataSocket.recv(512)
             if not data:
+                rospy.loginfo('Connection is lost')
                 break
             
             command = data.decode('utf-8')
@@ -60,10 +65,15 @@ if __name__ == '__main__':
             elif command == 'grasp':
                 pass
                 
+            encode_params = [cv2.IMWRITE_PNG_COMPRESSION, 8]
+            result, image_encode = cv2.imencode('.png', image, encode_params)
+            data = np.array(image_encode)
+            data = data.tobytes()
+            dataSocket.send(str(len(data)).ljust(16).encode())
             dataSocket.send(data)
         
         dataSocket.close()
         listenSocket.close()
         
     except (ConnectionError, OSError):
-        print('Connection failed')
+        rospy.logerr('Connection failed')
